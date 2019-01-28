9b9050fee0709e456dab9c11340e7637db5eb8bd
oscaroceguera <oscaroceguera@gmail.com>
2019-01-28 00:38:04 -0600

    Creating nested layout components

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/node_modules/gatsby-plugin-typography/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-about-js": () => import("/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-contact-js": () => import("/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-js": () => import("/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/.cache/data.json")
+

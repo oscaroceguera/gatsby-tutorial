6589bd4cd2f2d11de359df305c40e97cad44a04b
oscaroceguera <oscaroceguera@gmail.com>
2019-01-28 01:15:35 -0600

    Programmatically create pages from data

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,6 +2,7 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
+  "component---src-templates-blog-post-js": () => import("/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/src/templates/blog-post.js" /* webpackChunkName: "component---src-templates-blog-post-js" */),
   "component---cache-dev-404-page-js": () => import("/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-about-js": () => import("/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
   "component---src-pages-contact-js": () => import("/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/src/templates/blog-post.js"))),
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/.cache/dev-404-page.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/src/pages/about.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/Users/oscaroceguera/Documents/Repos/Examples/gatsby-tutorial/src/pages/contact.js"))),
